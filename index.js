2b3fb6812a0cee73ab07caf09db541f0f342e0a3
Beyhan <beyhan.taylan@hotmail.com>
2022-05-13 15:56:43 -0400

    commit tree e109f89777a51d79f59a2deee29eba6b68db9fd6

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+function findMatching(source, sought){
+    return source.filter(
+        possibleMatch => possibleMatch.toUpperCase() === sought.toUpperCase()  
+    )
+}
+function fuzzyMatch(source, testString){
+    return source.filter(
+        possibleMatch => 
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+
+}
+
+function matchName(source, sought){
+    return source.filter(
+        possibleMatch => {
+            
+           return possibleMatch.name.toLowerCase() === sought.toLowerCase() 
+        }
+    ) 
+
+}
